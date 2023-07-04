6c29ecf7597a67b107d9261a072ff43ccd537c18
Emmanuel Herrera <emherrer1@gmail.com>
2023-07-03 23:58:47 -0400

    add url, topic, language, limit and subject

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
 from sent_email import send_email
 
+# Get Topic
+topic = "Data engineer project"
+language = "en"
+
 api_key = "6d93aeca3b6e47359757fb74f60f9b20"
-url = "https://newsapi.org/v2/everything?q=tesla" \
-    "&from=2023-06-03&sortBy=publishedAt&apiKey" \
-    "=6d93aeca3b6e47359757fb74f60f9b20"
+url = "https://newsapi.org/v2/everything?" \
+    f"q={topic}&" \
+    "sortBy=publishedAt&" \
+    "apiKey=6d93aeca3b6e47359757fb74f60f9b20&" \
+    f"language={language}"
 
 # Make request
 request = requests.get(url)
@@ -13,12 +19,13 @@ request = requests.get(url)
 content = request.json()
 
 # Access articles titles and descriptions
-message = ""
-for article in content.get("articles"):
-    if article.get("title") and article.get("description") is not None:
-        message += article.get("title") + "\n" + article.get("description") + 2*"\n"
+message = "Subject: Today's news about" + " " + topic + "\n"
+
+# if content.get("articles") is not None:
+for article in content.get("articles")[:20]:
+    if (article.get("title") is not None) and (article.get("description") is not None):
+        message += article.get("title") + "\n" + article.get(
+            "description") + "\n" + article.get("url") + 2*"\n"
 
 message = message.encode("utf-8")
 send_email(message=message)
-
-
